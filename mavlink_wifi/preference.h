d3561726a8c1c79f30e41d83822296f4bae97be0
kurosuke <5730676+kurosuke56@users.noreply.github.com>
2026-04-03 18:54:16 +0900

    Update preference.h

--- mavlink_wifi/preference.h
+++ mavlink_wifi/preference.h
@@ -1,9 +1,9 @@
 // ===== WiFi設定 =====
-const char* WIFI_SSID = "your_ssid";
-const char* WIFI_PASSWORD = "your_password";
+const char* WIFI_SSID = "ssid2";
+const char* WIFI_PASSWORD = "pass";
 
 
 // ===== Mission Planner ホストIP =====
-const char* GROUND_STATION_IP = "your_PC_IPAddress";
+const char* GROUND_STATION_IP = "192.168.0.2";
 
 
